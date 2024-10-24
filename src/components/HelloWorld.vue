4ff52277fab0a207c1c2733c9ae88e606d5be474
FengXiWuTong <2366404545@qq.com>
2024-10-24 09:37:56 +0800

    20241024本地数据库更新

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,60 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
+
+// import { userRequest } from '../api/user'
 import axios from 'axios';
+defineProps<{ msg: string }>()
 let message = ref('');
+let localValue = ref('请输入SQL')
+let dataList = ref([])
+const databaseTest = () => {
+  (window as any).ipcRenderer.send('openFlyCar')
+}
 const addHttp = () => {
   axios.get('http://127.0.0.1:3000/api/test')
-    .then((response: any) => {
+    .then((response) => {
       // 请求成功，打印响应数据
       console.log('res: ', response.data);
-      message.value = '调用成功'
+      dataList.value = response.data
+      // 假设你有一个响应式变量dataList来存储数据
+      // dataList.value = response.data; // 这里需要根据你的实际代码来设置
     })
-    .catch((error: any) => {
+    .catch((error) => {
       // 请求失败，打印错误信息
       console.error('Error during fetching user data:', error);
     });
 }
-const sendMessage = () => {
-  (window as any).ipcRenderer.send('test', '向主线程调用消息');
-};
+const changeSQ = () => {
+  console.log(localValue, "localValue");
+  (window as any).ipcRenderer.send('changeSQ', localValue.value)
+}
 (window as any).ipcRenderer.on('add', (event: any, data: any) => {
   console.log(event, data)
   message.value = JSON.stringify(data)
 })
+
 </script>
 
 <template>
-  <div>
-    <button @click="sendMessage">渲染线程发送消息</button>
-    <button @click="addHttp">调用接口</button>
-    <p>{{ message }}</p>
+  <div class="card">
+    <p>
+      <button @click="databaseTest()">调取sqlite</button>
+    </p>
+    <p>
+      <button @click="changeSQ()">执行SQL操作</button>
+    </p>
+    <p>-------------</p>
+    {{ message }}
+    <p>-------------</p>
+    <input v-model="localValue" />
+    <p>-------------</p>
+    <p>INSERT INTO user (id) VALUES (‘添加的ID’)</p>
+    <p>-------------</p>
+    <p>DELETE FROM user WHERE id = ‘随便一个想要删除的’;</p>
+    <p>-------------</p>
+    <p>UPDATE user SET id = ‘修改后ID’ WHERE id = ‘修改前ID’;</p>
+    <p><button @click="addHttp()">http请求</button></p>
+    <p>http：{{ dataList }}</p>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import vue from '@vitejs/plugin-vue'
 
+// 安装 vite-plugin-electron 的时候，就同时安装了 vite-plugin-electron-renderer
+import electronRender from 'vite-plugin-electron-renderer'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
@@ -25,6 +29,13 @@ export default defineConfig({
         // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
         ? undefined
         : {},
-    }),
+    }), electronRender()
   ],
+  build: {
+    rollupOptions: {
+      external: ['sqlite3'],  // 如果使用 sqlite3，请确保这里的包名正确
+    },
+  },
 })
+
+
